01d6cc043125485031317834fd4f9057ad65c283
Ziru Zou <jiyucoding@yahoo.com>
2022-12-19 16:48:47 -0500

    commit tree fda2f36d59991566e10d61df101551ecfc9f324a

---
+++ index.js
@@ -0,0 +1,46 @@
+// Create the add()
+function add(a, b) {
+    // return 767   // hard coding, never do that!
+    return a + b
+}
+
+
+// Create the subtract()s
+function subtract(a, b) {
+    return a - b
+}
+
+// Create the multiply()
+function multiply(a, b) {
+    return a * b
+}
+
+
+// Create the divide()
+function divide(a, b) {
+    return a / b;
+}
+
+
+// Create the increment()
+function increment(a) {
+    return a += 1
+}
+
+
+// Create the decrement()
+function decrement(a) {
+    return a -= 1;
+}
+
+
+// Create the makeInt()
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+
+// Create the preserveDecimal()
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
